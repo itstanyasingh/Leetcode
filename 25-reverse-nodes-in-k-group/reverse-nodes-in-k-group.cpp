8b883c88497e98b9380653f1772b8f31695f7883
Tanya Singh <tanyasingh29600@gmail.com>
2026-07-19 14:41:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (39.58%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,43 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : next(next), val(x) {}
- * };
- */
-
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
 
-        // Check if k nodes are available
-        ListNode* temp = head;
-        for(int i = 0; i < k; i++)
-        {
-            if(temp == NULL)
-                return head;
+        ListNode dummy(0);
+        dummy.next = head;
 
-            temp = temp->next;
+        ListNode* prevGroup = &dummy;
+
+        while (true) {
+
+            // Find kth node
+            ListNode* kth = prevGroup;
+            for (int i = 0; i < k && kth != nullptr; i++)
+                kth = kth->next;
+
+            if (kth == nullptr)
+                break;
+
+            ListNode* groupNext = kth->next;
+
+            // Reverse current group
+            ListNode* prev = groupNext;
+            ListNode* curr = prevGroup->next;
+
+            while (curr != groupNext) {
+                ListNode* temp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            ListNode* temp = prevGroup->next;
+            prevGroup->next = kth;
+            prevGroup = temp;
         }
 
-        // Reverse first k nodes
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-
-        for(int i = 0; i < k; i++)
-        {
-            ListNode* nextNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-
-        // Connect with remaining list
-        head->next = reverseKGroup(curr, k);
-
-        return prev;
+        return dummy.next;
     }
 };
