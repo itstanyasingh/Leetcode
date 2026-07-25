a52424874ffc5dae69a14e0e74dd034ff42d3ce5
Tanya Singh <tanyasingh29600@gmail.com>
2026-07-25 10:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (60.43%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> subset;
+
+    void solve(vector<int>& nums, int index) {
+        if (index == nums.size()) {
+            ans.push_back(subset);
+            return;
+        }
+
+        // Include current element
+        subset.push_back(nums[index]);
+        solve(nums, index + 1);
+
+        // Backtrack
+        subset.pop_back();
+
+        // Exclude current element
+        solve(nums, index + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        solve(nums, 0);
+        return ans;
+    }
+};
